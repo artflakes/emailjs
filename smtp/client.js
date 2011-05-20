e3b5938d4a15e5d6a8ec47f71b8e14877dde7e72
Daniel Schweighoefer <acid@acidforce.org>
2011-05-20 16:16:45 +0200

    frustrating, but it's working this way

--- smtp/client.js
+++ smtp/client.js
@@ -1,14 +1,14 @@
-var smtp 		= require('./smtp');
-var smtpError 	= require('./error');
-var message		= require('./message');
-var address		= require('./address');
-var oil		= require("mysql-oil");
+var smtp     = require('./smtp');
+var smtpError   = require('./error');
+var message    = require('./message');
+var address    = require('./address');
+var oil    = require("mysql-oil");
 var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 /*
 __UTC__ version for
   [formatting Date for mySQL](https://github.com/rentzsch/node-mysql-oil/blob/master/lib/node-mysql-oil.js#L237)
   heavily modified
-*/  
+*/
 var dbTimestamp = function() {
   var date, day, month, time, zero, x;
   x = new Date();
@@ -29,13 +29,13 @@ var dbTimestamp = function() {
 
 var Client = function(server, db_config)
 {
-	this.smtp			= new smtp.SMTP(server);
+  this.smtp      = new smtp.SMTP(server);
 
-	//this.smtp.debug(1);
+  //this.smtp.debug(1);
 
-	this.queue			= [];
-	this.timer			= null;
-	this.sending		= false;
+  this.queue      = [];
+  this.timer      = null;
+  this.sending    = false;
 
   if (typeof(db_config)==='object')
     oil = oil.connect(db_config);
@@ -45,78 +45,78 @@ var Client = function(server, db_config)
 
 Client.prototype = 
 {
-	_poll: function()
-	{
-		var self = this;
+  _poll: function()
+  {
+    var self = this;
 
-		clearTimeout(self.timer);
+    clearTimeout(self.timer);
 
-		if(self.queue.length)
-		{
-			if(self.smtp.state() == smtp.state.NOTCONNECTED)
-				self._connect(self.queue[0]);
+    if(self.queue.length)
+    {
+      if(self.smtp.state() == smtp.state.NOTCONNECTED)
+        self._connect(self.queue[0]);
 
-			else if(self.smtp.state() == smtp.state.CONNECTED && !self.sending)
-				self._sendmail(self.queue.shift());
-		}
-		// wait around 1 seconds in case something does come in, otherwise close out SMTP connection
-		else
-			self.timer = setTimeout(function() { self.smtp.quit(); }, 1000);
-	},
+      else if(self.smtp.state() == smtp.state.CONNECTED && !self.sending)
+        self._sendmail(self.queue.shift());
+    }
+    // wait around 1 seconds in case something does come in, otherwise close out SMTP connection
+    else
+      self.timer = setTimeout(function() { self.smtp.quit(); }, 1000);
+  },
 
-	_connect: function(stack)
-	{
-		var self = this,
+  _connect: function(stack)
+  {
+    var self = this,
 
-		connect = function(err)
-		{
-			if(!err)
-			{
-				var login = function(err)
-				{
-					if(!err)
-						self._poll();
+    connect = function(err)
+    {
+      if(!err)
+      {
+        var login = function(err)
+        {
+          if(!err)
+            self._poll();
 
-					else
-						stack.callback(err, stack.message);
-				};
+          else
+            stack.callback(err, stack.message);
+        };
 
-				if(!self.smtp.authorized())
-					self.smtp.login(login);
+        if(!self.smtp.authorized())
+          self.smtp.login(login);
 
-				else
-					self._poll();
-			}
-			else
-				stack.callback(err, stack.message);
-		};
+        else
+          self._poll();
+      }
+      else
+        stack.callback(err, stack.message);
+    };
 
-		self.smtp.connect(connect);
-	},
+    self.smtp.connect(connect);
+  },
 
-	send: function(msg, callback)
-	{
-		var self = this;
+  send: function(msg, callback)
+  {
+    var self = this;
 
-		if(!(msg instanceof message.Message) && msg.from && msg.to && msg.text)
-			msg = message.create(msg);
+    if(!(msg instanceof message.Message) && msg.from && msg.to && msg.text)
+      msg = message.create(msg);
 
-		if(msg instanceof message.Message && msg.valid())
-		{
-			var stack = 
-			{
-				message:		msg,
-				to:			address.parse(msg.header["to"]),
-				from:			address.parse(msg.header["from"])[0].address,
-				callback:	callback || function() {},
+    if(msg instanceof message.Message && msg.valid())
+    {
+      var stack = 
+      {
+        message:    msg,
+        to:      address.parse(msg.header["to"]),
+        from:      address.parse(msg.header["from"])[0].address,
+        callback:  callback || function() {},
         uuid: msg.header["uuid"] ? msg.header["uuid"] : ''
-			};
+      };
 
-			if(msg.header["cc"])
-				stack.to = stack.to.concat(address.parse(msg.header["cc"]));
+      if(msg.header["cc"])
+        stack.to = stack.to.concat(address.parse(msg.header["cc"]));
 
-			if(msg.header["bcc"])
-				stack.to = stack.to.concat(address.parse(msg.header["bcc"]));
+      if(msg.header["bcc"])
+        stack.to = stack.to.concat(address.parse(msg.header["bcc"]));
 
       oil({ insert_into: 'mailqueue',
             values: { created_at: dbTimestamp(),
@@ -128,8 +128,6 @@ Client.prototype =
               if(!err)
               {
                 stack["db_id"] = info.insertId;
-                self.queue.push(stack);
-                self._poll();
               }
               else
               {
@@ -139,56 +137,58 @@ Client.prototype =
               }
             })
       });
-			
-		}
-		else
-			callback({code:-1, message:"message is not a valid Message instance"}, msg);
-	},
-	_sendsmtp: function(stack, next)
-	{
-		var self	= this;
-		var check= function(err)
-		{
-			if(!err && next)
-				next.apply(self, [stack]);
+      
+      self.queue.push(stack);
+      self._poll();
+    }
+    else
+      callback({code:-1, message:"message is not a valid Message instance"}, msg);
+  },
+  _sendsmtp: function(stack, next)
+  {
+    var self  = this;
+    var check= function(err)
+    {
+      if(!err && next)
+        next.apply(self, [stack]);
 
-			else
-				stack.callback(err, stack.message);
-		};
+      else
+        stack.callback(err, stack.message);
+    };
 
-		return check;
-	},
+    return check;
+  },
 
-	_sendmail: function(stack)
-	{
+  _sendmail: function(stack)
+  {
     var self = this;
     self.sending = true;
     self.smtp.mail(self._sendsmtp(stack, self._sendrcpt), '<' + stack.from + '>');
-	},
+  },
 
-	_sendrcpt: function(stack)
-	{
-		var self = this, to = stack.to.shift().address;
-		self.smtp.rcpt(self._sendsmtp(stack, stack.to.length ? self._sendrcpt : self._senddata), '<'+ to +'>');
-	},
+  _sendrcpt: function(stack)
+  {
+    var self = this, to = stack.to.shift().address;
+    self.smtp.rcpt(self._sendsmtp(stack, stack.to.length ? self._sendrcpt : self._senddata), '<'+ to +'>');
+  },
 
-	_senddata: function(stack)
-	{
-		var self = this;
-		self.smtp.data(self._sendsmtp(stack, self._sendmessage));
-	},
+  _senddata: function(stack)
+  {
+    var self = this;
+    self.smtp.data(self._sendsmtp(stack, self._sendmessage));
+  },
 
-	_sendmessage: function(stack)
-	{
-		var self = this, stream = stack.message.stream();
+  _sendmessage: function(stack)
+  {
+    var self = this, stream = stack.message.stream();
 
-		stream.on('data', function(data) { self.smtp.message(data); });
-		stream.on('end', function() { self.smtp.data_end(self._sendsmtp(stack, self._senddone)); });
-		stream.on('error', self._sendsmtp(stack));
-	},
+    stream.on('data', function(data) { self.smtp.message(data); });
+    stream.on('end', function() { self.smtp.data_end(self._sendsmtp(stack, self._senddone)); });
+    stream.on('error', self._sendsmtp(stack));
+  },
 
-	_senddone: function(stack)
-	{
+  _senddone: function(stack)
+  {
     oil({ update: 'mailqueue',
       values: { send_at: dbTimestamp() },
       where: ['id = ?', stack.db_id],
@@ -205,6 +205,6 @@ exports.Client = Client;
 
 exports.connect = function(server, dbconfig)
 {
-	return new Client(server, dbconfig);
+  return new Client(server, dbconfig);
 }
 
